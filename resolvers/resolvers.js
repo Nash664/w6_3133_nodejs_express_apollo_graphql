bff34b2e56cb4768d7134f64412d25e35c7f767a
Nash664 <nashirferoz01@gmail.com>
2026-02-22 12:10:43 -0800

    Add movie schema and resolvers

--- resolvers/resolvers.js
+++ resolvers/resolvers.js
@@ -1,3 +1,27 @@
-import MovieModel from '../models/movie.js';
+import MovieModel from '../models/Movie.js';
+
+const buildUpdatePayload = (payload) =>
+  Object.fromEntries(
+    Object.entries(payload).filter(([, value]) => typeof value !== 'undefined')
+  );
 
 // Resolvers define the technique for fetching the types defined in the schema.
+const movieResolvers = {
+  Query: {
+    movies: async () => MovieModel.find(),
+    movie: async (_, { id }) => MovieModel.findById(id),
+    moviesByDirector: async (_, { director_name }) =>
+      MovieModel.findByDirector(director_name),
+  },
+  Mutation: {
+    addMovie: async (_, args) => MovieModel.create(args),
+    updateMovie: async (_, { id, ...updates }) =>
+      MovieModel.findByIdAndUpdate(id, buildUpdatePayload(updates), {
+        new: true,
+        runValidators: true,
+      }),
+    deleteMovie: async (_, { id }) => MovieModel.findByIdAndDelete(id),
+  },
+};
+
+export default movieResolvers;
